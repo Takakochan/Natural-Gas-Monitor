af12f5b65cf6005d5e0eae2ee9675459dac60522
TKK <73033322+Takakochan@users.noreply.github.com>
2025-04-12 10:31:38 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from matplotlib.pylab import *
+import os
+
+html = os.path.join('data', 'gas.html')
+output_csv = os.path.join('data', 'ngas.csv')
+
+tables = pd.read_html(html)
+
+
+df = tables[0]
+tables[0].to_csv('/Users/takako/Desktop/ngas.csv', index=False)
+gas = tables[0]
+gas = gas.drop('Unnamed: 5', axis=1)
+gas = gas.dropna()
+
+gas['Actual'] = gas['Actual'].str.extract(r'(-?\d+)').astype(int)
+gas['Forecast'] = gas['Forecast'].str.extract(r'(-?\d+)').astype(int)
+gas = gas.reset_index()
+gas = gas.sort_index(ascending=False)
+
+price = pd.read_csv(output_csv)
+price = price.sort_index(ascending=False)
+df2 = price['Price'].tail(50)
+df1 = gas.tail(50)
+gas = pd.concat([df1, df2], axis=1)
+print(gas)
+
+fig = plt.figure()
+ax = gas.tail(30).plot(x='Release Date',
+                       y=['Actual', 'Forecast'],
+                       title='Natural Gas EIA & Price')
+gas.tail(29).plot(x='Release Date', y=['Price'], secondary_y=True, ax=ax)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, fontsize=10)
+plt.show()
+print(gas.tail(50))
